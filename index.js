81f3d3e1c698926c026295ad17e8ad2491b26d43
DucBEDev <ducpham160604@gmail.com>
2024-02-27 14:50:06 +0700

    Set up file

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const route = require('./routes/client/index.route')
-const app = express();
-const port = 3000;
+// Connect to env
+require("dotenv").config();
 
+// Connect to ExpressJS
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+// Connect to mongoose DB
+const database = require("./config/database")
+database.connect();
+
+// Connect to routes
+const route = require('./routes/client/index.route')
+route(app);
+
+// Connect to pug
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-// Route
-route(app);
+// Configuration public file
+app.use(express.static("public"));
 
 
 app.listen(port, () => {
